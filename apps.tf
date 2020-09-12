18be2be77cccec90badae0b40b9504608c910c31
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2020-09-12 13:16:29 -0300

    Create state for terraform

--- apps.tf
+++ apps.tf
@@ -7,7 +7,7 @@ resource "heroku_app" "staging" {
   config_vars = {
     APP_ENV = "staging",
     ASPNETCORE_ENVIRONMENT = "staging",
-   "ConnectionStrings:Default" = "Host=ec2-3-226-231-4.compute-1.amazonaws.com;Port=5432;Username=gpjcaofzeobuiu;Password=a3a90a47afd5576e33adee3dc7202de733cbf786687e96a328eeaa7eaaec923e;Database=d1uvlefds4j0ci;",
+   "ConnectionStrings:Default" = "change_me",
    "JWT:Secret" = "4ad83c88c8676f939ac98756d8049822b417e4d20a89675b9ca4e4df76b022c69280646b0754e4e5cea066db427150fbf852814d0ae0dea7a2b486fff063bde2",
    "JWT:ValidAudience" = "http://localhost:4200",
    "JWT:ValidIssuer" = "http://localhost:61955",
